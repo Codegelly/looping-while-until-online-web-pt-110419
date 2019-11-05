0861f0a56d2f097125615060cb0d2ffab73bdd74
Angelica  Thompson <i.thompson89@outlook.com>
2019-11-05 16:31:53 +0000

    Automatically backed up by Learn

--- lib/while.rb
+++ lib/while.rb
@@ -1,15 +1,16 @@
+#def using_while
+ # levitation_force = 6
+#  while levitation_force < 10
+#	puts "Wingardium Leviosa"
+
+#end
+#end
+
+
 def using_while
-  levitation_force = 6
-  while levitation_force < 10
-	puts "Wingardium Leviosa"
-
-end
-end
-
-
-
 levitation_force = 6
 while levitation_force < 10
   puts "#{levitation_force}"
   levitation_force += 1
 end
+end 
